90a6348d85e3adccf567d2a952c77b3fec8404bc
lupomikti <25500382+lupomikti@users.noreply.github.com>
2023-12-03 11:11:16 -0800

    Fix: implementation of sort by locked state then score

--- wanikani-similar-kanji/wk_niai.main.js
+++ wanikani-similar-kanji/wk_niai.main.js
@@ -107,48 +107,6 @@ function WK_Niai()
         return result;
     };
 
-    const sort_by_locked_score = function(kanjiA, kanjiB)
-    {
-        // kanjis not in DB -> move to end
-        const kanjiA_inDB = this.ndb.isKanjiInDB(kanjiA);
-        const kanjiB_inDB = this.ndb.isKanjiInDB(kanjiB);
-        if (!kanjiA_inDB && !kanjiB_inDB)
-            return 0;
-        else if (kanjiA_inDB && !kanjiB_inDB)
-            return -1;
-        else if (!kanjiA_inDB && kanjiB_inDB)
-            return 1;
-
-        // kanjis locked should be shown last
-        // note: treat kanjis not in WK as locked too
-        const kanjiA_islocked = this.ndb.isKanjiInWK(kanjiA) ? this.ndb.isKanjiLocked(kanjiA,this.settings.user_level) : true;
-        const kanjiB_islocked = this.ndb.isKanjiInWK(kanjiB) ? this.ndb.isKanjiLocked(kanjiB,this.settings.user_level) : true;
-        const kanjiA_score = this.ndb.getInfo(kanjiA).score;
-        const kanjiB_score = this.ndb.getInfo(kanjiB).score;
-        if (!kanjiA_islocked && !kanjiB_islocked)
-        {
-            // both kanjis are unlocked, so sort them by score
-            if (kanjiA_score < kanjiB_score)
-                return 1;
-            if (kanjiA_score > kanjiB_score)
-                return -1;
-            return 0;
-        }
-        else if (kanjiA_islocked && !kanjiB_islocked)
-            return 1; // move locked kanji to end
-        else if (!kanjiA_islocked && kanjiB_islocked)
-            return -1; // move locked kanji to end
-        else
-        {
-            // both kanjis are locked, so sort them by score
-            if (kanjiA_score < kanjiB_score)
-                return 1;
-            if (kanjiA_score > kanjiB_score)
-                return -1;
-            return 0;
-        }
-    };
-
     // #########################################################################
     WK_Niai.prototype.populateNiaiSection = function(kanji, curPage = this.wkItemInfoRef.currentState.on)
     {
@@ -159,11 +117,7 @@ function WK_Niai()
         if (this.settings.use_alt)
             use_sources = [...this.settings.alt_sources, ...use_sources];
 
-        // sort similar kanji by: score, locked status
-        var similar_kanji = this.ndb.getSimilar(kanji,this.settings.user_level,use_sources,this.settings.min_score);
-        similar_kanji.sort(sort_by_locked_score.bind(this));
-
-        const similar_list = [kanji,...similar_kanji];
+        const similar_list = [kanji, ...this.ndb.getSimilar(kanji,this.settings.user_level,use_sources,this.settings.min_score)];
         let char_list = [];
         similar_list.forEach(
             function(sim_kanji)
@@ -200,7 +154,7 @@ function WK_Niai()
         $(`#niai_similar_grid`).html(char_list.map(this.gen_item_chargrid).join(``));
 
         if (typeof wkof === `object`)
-            this.update_wk_cache(similar_list);
+            this.update_wk_cache(similar_list); // may not be needed anymore with changes to NiaiDB
 
         if (!$(`#niai_badges_btn i`).hasClass(`fa-times-circle-o`))
         {
@@ -229,10 +183,10 @@ function WK_Niai()
     // #########################################################################
 
     // #########################################################################
-    WK_Niai.prototype.init = function()
+    WK_Niai.prototype.init = async function()
     {
         GM_addStyle(GM_getResourceText(`niai_style`)
-                        .replace(/\.wk_namespace/g, `#niai_section`));
+                        .replace(/wk_namespace/g, GM_info.script.namespace));
 
         this.settings.debug      = GM_getValue(`debug`)      || false;
         this.settings.minify     = GM_getValue(`minify`)     || false;
@@ -262,7 +216,7 @@ function WK_Niai()
             } :
             function() {};
 
-        this.ndb.init(this.override_db);
+        await this.ndb.init(this.override_db);
 
         this.log(`The script element is:`, GM_info);
         this.log("The override db is", this.override_db);
@@ -313,8 +267,7 @@ let promise = typeof wkof !== `undefined` ? (wkof.include(`Jquery, Apiv2`), wkof
 promise.then(() => {
     const wk_niai = new WK_Niai();
 
-    wk_niai.init();
-    wk_niai.run();
+    wk_niai.init().then(() => wk_niai.run());
 });
 // #############################################################################
 // #############################################################################
